1a6f56c1f7386c5f89b743c8423f012b2e7583d8
developersaima <saimasattar6040@gmail.com>
2026-02-25 18:40:53 +0600

    link JS and setup basic functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+const tabs = document.querySelectorAll(".tab");
+const cards = document.querySelectorAll(".job-card");
+const interviewCount = document.getElementById("interviewCount");
+const rejectedCount = document.getElementById("rejectedCount");
+const totalCount = document.getElementById("totalCount");
+const jobTabCount = document.getElementById("jobTabCount");
+const emptyState = document.querySelector(".empty-state");
+
+let activeTab = "all";
+
